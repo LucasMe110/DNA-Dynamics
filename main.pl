5ef6d9a4514d049ff9045e4c059a9fdd48e12d39
Lucas Mello <lucasmellomo@gmail.com>
2024-10-28 12:04:58 -0300

    json

--- main.pl
+++ main.pl
@@ -2,12 +2,17 @@
 
 use strict;
 use warnings;
+use JSON; # Importa o módulo JSON para manipulação de arquivos JSON
 
-# Nome do arquivo CSV a ser lido
+# Nome do arquivo CSV a ser lido e do JSON a ser criado
 my $arquivo_csv = 'DNA.csv';
+my $arquivo_json = 'DNA_output.json';
 
 # Abrir o arquivo CSV
 open(my $fh, '<', $arquivo_csv) or die "Não foi possível abrir o arquivo '$arquivo_csv' $!";
+
+# Array para armazenar as informações de cada sequência de DNA
+my @dados_dna;
 
 # Loop através das linhas do arquivo
 while (my $linha = <$fh>) {
@@ -37,7 +42,30 @@ while (my $linha = <$fh>) {
     print "Percentual GC: $pc_gc%\n";
 
     print "----------\n";
+
+    # Armazena as informações da sequência em um hash
+    my %sequencia_info = (
+        dna_sequence => $dna,
+        rna_sequence => $rna,
+        counts       => {
+            A => $a_count,
+            T => $t_count,
+            G => $g_count,
+            C => $c_count,
+        },
+        gc_percentage => $pc_gc,
+    );
+
+    # Adiciona o hash ao array de dados
+    push @dados_dna, \%sequencia_info;
 }
 
 # Fecha o arquivo após a leitura
 close($fh);
+
+# Converter os dados para JSON e salvar no arquivo
+open(my $json_fh, '>', $arquivo_json) or die "Não foi possível criar o arquivo JSON '$arquivo_json' $!";
+print $json_fh encode_json(\@dados_dna);
+close($json_fh);
+
+print "As informações foram salvas no arquivo JSON '$arquivo_json'.\n";
